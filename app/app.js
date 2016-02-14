3a436bd93c9225ba1f303ffb4d51e5b6ac1fb0f1
Elliott Hindman <portokaliajapan@gmail.com>
2016-02-15 00:34:17 +0900

    begin implementing database logic

--- app/app.js
+++ app/app.js
@@ -1,27 +1,39 @@
 'use strict';
 
-var app = angular.module('sbApp', ['ngRoute', 'ngCookies', 'firebase']);
+var app = angular
+    .module('sbApp', [
+        'ngRoute', 
+        'ngCookies', 
+        'firebase'
+    ])
+    .constant('FURL', 'https://sbapp1122.firebaseio.com/')  
+    .config(function($locationProvider, $routeProvider) {
+       $routeProvider
+            .when('/', {
+                templateUrl: 'html/home.html'
+            })
 
-app.config(function($locationProvider, $routeProvider) {
 
-    // Routes
-    $routeProvider
-        .when('/', {
-            templateUrl: 'html/home.html'
-        })
-        .when('/topic', {
-            templateUrl: 'html/topic.html'
-        })
-        .when('/post', {
-            templateUrl: 'html/post.html'
-        })
-        .when('/profile', {
-            templateUrl: 'html/profile.html'
-        })
-        .otherwise({
-            redirectTo: '/',
-        });
-    $locationProvider
-        .html5Mode(true);
+            //- Study Buddy Routes
+            .when('/topic', {
+                templateUrl: 'html/topic.html'
+            })
+            .when('/create', {
+                templateUrl: 'html/create.html'
+            })
 
+
+
+            //- User Routes
+            .when('/profile', {
+                templateUrl: 'html/profile.html'
+            })
+
+            //- CatchAll
+            .otherwise({
+                redirectTo: '/'
+            });
+
+        $locationProvider
+            .html5Mode(true);
 });
--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,26 +1,30 @@
 'use strict';
 
-app.controller('MainController', ['$scope', '$location', '$cookies', '$firebaseObject', '$firebaseArray', function ($scope, $location, $cookies, $firebaseObject, $firebaseArray) {
+app.controller('MainController', ['$scope', '$location', '$firebaseObject', '$firebaseArray', function ($scope, $location, $firebaseObject, $firebaseArray) {
     
     // Setup Firebase base reference
     var ref = new Firebase('https://sbapp1122.firebaseio.com/');
 
     // Create date string
-    $scope.date = moment().format('MMMM Do, YYYY');
-
-    $scope.tempcoverimage = "https://static.pexels.com/photos/45917/pexels-photo-45917-large.jpeg"
+    // $scope.date = moment().format('MMMM Do, YYYY');
 
     // Login User
     var user = ref.child('users').child('ethindman');
     var syncUser = $firebaseObject(user);
     syncUser.$bindTo($scope, "user");
 
-    var buddies = ref.child('buddies').child('ethindman');
-    var syncBuddy = $firebaseObject(buddies);
-    syncBuddy.$bindTo($scope, "buddies");
+    // var buddies = ref.child('buddies').child('ethindman');
+    // var syncBuddy = $firebaseObject(buddies);
+    // syncBuddy.$bindTo($scope, "buddies");
 
-    $scope.createBuddy = function() {
-        alert("got here");
+
+    // BUDDY LOGIC
+    var buddies = ref.child('buddies').child('ethindman');
+    var data = $firebaseArray(buddies);
+
+    $scope.createBuddy = function(buddy) {
+        buddy.created_on = Firebase.ServerValue.TIMESTAMP;
+        return data.$add(buddy);
     }
     
 }]);
