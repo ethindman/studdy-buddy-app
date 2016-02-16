1ddae7460bde2cee14a33e29e0bbe373844feb9b
Elliott Hindman <portokaliajapan@gmail.com>
2016-02-17 00:13:27 +0900

    adds simple registration

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -18,32 +18,65 @@ app.controller('MainController', ['$scope', 'FURL', 'Auth', '$firebaseObject',
 ]);
 
 // Factory for generating re-useable $firebaseAuth instance
-app.factory("Auth", ["$firebaseAuth", "FURL",
-  function($firebaseAuth, FURL) {
+app.factory('Auth', function($firebaseAuth, $firebaseArray, FURL) {
+    
     var ref = new Firebase(FURL);
-    return $firebaseAuth(ref);
-  }
-]);
+    var authRef = $firebaseAuth(ref);
+
+    var Auth = {
+
+        user: {},
+
+        register: function(user) {
+            return authRef.$createUser({
+                email: user.email,
+                password: user.password
+            }).then(function() {                
+                return Auth.login(user);
+            }).then(function(data) {
+                return Auth.createProfile(data.uid, user);
+            });
+        },
+
+        login: function(user) {
+            return authRef.$authWithPassword({
+                email: user.email,
+                password: user.password
+            });
+        },
+
+        createProfile: function(uid, user) {
+            var profile = {
+                full_name: user.full_name,
+                email: user.email
+            };
+
+            var profileRef = $firebaseArray(ref.child('profile').child(uid));
+            
+            return profileRef.$add(profile);
+        }
+
+    };
+
+    return Auth;
+
+});
 
 // User Controller
 app.controller('UserController', ['$scope', 'Auth',
     function ($scope, Auth) {
 
         // NEW USER
-        $scope.register = function() {
+        $scope.register = function(user) {
             $scope.message = null;
             $scope.error   = null;
 
-            Auth.$createUser({
-                full_name: $scope.full_name,
-                email: $scope.email,
-                password: $scope.password
-            }).then(function(userData) {
-                $scope.message = "User created with uid: " + userData.uid;  
-            }).catch(function(error) {
-                $scope.error = error;
-            });
+            Auth.register(user)
+                .then(function(userData) {
+                    $scope.message = "User created with uid: " + userData.uid;  
+                }).catch(function(error) {
+                    $scope.error = error;
+                });
         };
-
     }
 ]);
