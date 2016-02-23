5f792f19a8671a920139258a2defa0ea32601ba8
Elliott Hindman <portokaliajapan@gmail.com>
2016-02-23 22:50:33 +0900

    clean up login/registration

--- app/js/controllers/AuthsController.js
+++ app/js/controllers/AuthsController.js
@@ -4,20 +4,20 @@ app.controller('AuthsController', function($scope, $location, $firebaseObject, F
 
     $scope.register = function(user) {
         Auth.register(user)
-            .then(function(userData) {
-                console.log("Successfully registered");
-                $location.path('/profile');
-            }).catch(function(error) {
-                console.log(error); 
-            });
+        .then(function(authData) {
+            $location.path('/profile');
+        }).catch(function(error) {
+            console.log(error); 
+        });
     };
 
     $scope.login = function(user) {
         Auth.login(user)
-          .then(function(authData) {
-            $location.path('/');
-          }, function(err) {        
-            console.log(err);
+        .then(function(authData) {
+            alert("Sign in Successful");
+            $location.path('/profile');
+        }).catch(function(error) {        
+            console.log(error);
         });   
     };
 
--- app/js/controllers/MainsController.js
+++ app/js/controllers/MainsController.js
@@ -1,7 +1,14 @@
-app.controller('MainsController', function ($scope, FURL, Auth) {
+app.controller('MainsController', function ($scope, $location, FURL, Auth) {
 
 	var ref = new Firebase(FURL);
 
 	$scope.user = Auth.user;
-		
+	$scope.signedIn = Auth.signedIn;
+	
+	$scope.logout = function() {
+		Auth.logout();
+		alert("Logout Successful!");
+		$location.path('/');
+	};
+
 });
