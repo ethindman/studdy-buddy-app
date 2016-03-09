5796a6a5162bd43f6f58bd7aebb5afaa74330981
Elliott Hindman <elliott_hindman@metaps.com>
2016-03-09 18:41:10 +0900

    fix login/registration pages and profile

--- app/js/controllers/AuthsController.js
+++ app/js/controllers/AuthsController.js
@@ -1,28 +1,28 @@
 app.controller('AuthsController', function($scope, $location, $firebaseObject, FURL, Auth) {
 
-    var ref = new Firebase(FURL);
-    $scope.user = Auth.user;
+  var ref = new Firebase(FURL);
+  $scope.user = Auth.user;
 
-    $scope.register = function(user) {
-        Auth.register(user)
-        .then(function(authData) {
-            toastr.success('Registration successful!');
-            $location.path('/profile');
-        }).catch(function(error) {
-            toastr.error('Oops! Something when wrong...');
-            console.log(error);
-        });
-    };
+  $scope.register = function(user) {
+    Auth.register(user)
+    .then(function(authData) {
+      toastr.success('Registration successful!');
+      $location.path('/profile');
+    }).catch(function(error) {
+      toastr.error('Oops! Something when wrong...');
+      console.log(error);
+    });
+  };
 
-    $scope.login = function(user) {
-        Auth.login(user)
-        .then(function(authData) {
-            toastr.success('Login successful!');
-            $location.path('/profile');
-        }).catch(function(error) {        
-            toastr.error('Oops! Something when wrong...');
-            console.log(error);
-        });   
-    };
+  $scope.login = function(user) {
+    Auth.login(user)
+      .then(function(authData) {
+        toastr.success('Login successful!');
+        $location.path('/feed');
+      }).catch(function(error) {        
+        toastr.error('Oops! Please check your email or password and try again...');
+        console.log(error);
+    });   
+  };
 
 });
--- app/js/controllers/FeedsController.js
+++ app/js/controllers/FeedsController.js
@@ -1,8 +1,12 @@
-app.controller('FeedsController', function($scope, $routeParams, Post, Auth) {
+app.controller('FeedsController', function($scope, $location, $routeParams, Post, Auth) {
   
   $scope.posts = Post.all;
   $scope.user = Auth.user;
   $scope.signedIn = Auth.signedIn;
+
+  if(!Auth.signedIn()) {
+    $location.path('/login');
+  }
 
   if($routeParams.postId) {
     $scope.post = Post.getPost($routeParams.postId);
